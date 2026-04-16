914fa6a61ae29eb9ceecd4d08d6b64fe47e48b51
miguelh612 <mi.villagra@duocuc.cl>
2026-04-16 16:25:15 -0400

    Se corrige configuración EC2.

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "network" {
   source = "./modules/network"
 }
 
-module "compute" {
+module "ec2" {
   source                            = "./modules/ec2"
   AUY1105-webapp-public-subnet-1_id = module.network.public_subnet_1
   AUY1105-webapp-public-subnet-2_id = module.network.public_subnet_2
---
+++ modules/ec2/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "AUY1105-webapp-ec2" {
+  ami           = var.ami_ubuntu
+  instance_type = var.instance_type
+  subnet_id     = var.AUY1105-webapp-public-subnet-1_id
+
+  key_name               = "vockey"
+  vpc_security_group_ids = [var.AUY1105-webapp-sg_ec2_id]
+  tags = {
+    "Name" = "AUY1105-webapp-ec2"
+  }
+}
