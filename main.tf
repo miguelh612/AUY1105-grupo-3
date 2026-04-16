5eb9469972e954ac7655aa2fafc2dd2b12de56b7
miguelh612 <mi.villagra@duocuc.cl>
2026-04-16 16:01:55 -0400

    Correcciones menores

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ module "network" {
 }
 
 module "compute" {
-  source           = "./modules/compute"
-  PublicSubnet1_id = module.network.public_subnet_1
-  PublicSubnet2_id = module.network.public_subnet_2
-  sg_frontend_id   = module.network.sg_ec2_id
-  vpc_1_id         = module.network.main_vpc_id
+  source                            = "./modules/ec2"
+  AUY1105-webapp-public-subnet-1_id = module.network.public_subnet_1
+  AUY1105-webapp-public-subnet-2_id = module.network.public_subnet_2
+  AUY1105-webapp-sg_ec2_id          = module.network.sg_ec2_id
+  AUY1105-webapp-vpc_id             = module.network.main_vpc_id
 }
