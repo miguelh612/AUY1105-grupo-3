75a4cf7509415d6d0f327042d5f6ed400a6d1dab
crlealb <cr.lealb@duocuc.cl>
2026-04-16 16:32:48 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,11 @@
 terraform {
+  required_version = ">= 1.0" 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.41.0"
     }
   }
-}
-rule "terraform_required_version" {
-  enabled = true
 }
 provider "aws" {
   region = "us-east-1"
